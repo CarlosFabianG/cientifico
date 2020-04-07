71bbaa6aa49fbd1f67cacb9dff61ed42c0c9e383
Carlos Fabián <carl.fab.g@gmail.com>
2020-04-07 00:22:51 -0500

    template de Character listo

---
+++ src/templates/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    const view = `
+    <div class="Header-main">
+        <div class="Header-logo">
+            <h1>
+                <a href="/">
+                    About
+                </a>
+            </h1>
+        </div>
+    </div>
+    `;
+    return view;
+}
+
+export default Header;
